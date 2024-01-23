a2647489909ec889afab7d1dfd743b1bdf0a9b68
Lucas Mendes <lucas39lm@hotmail.com>
2024-01-23 08:14:06 -0300

    Weather App

---
+++ src/WeatherAppGui.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class WeatherAppGui extends JFrame {
+    public WeatherAppGui(){
+        super("Weather App");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setSize(450, 650);
+
+        setLocationRelativeTo(null); //centraliza a tela
+
+        setLayout(null);
+
+        setResizable(false);
+    }
+}
