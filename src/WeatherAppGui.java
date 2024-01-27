8f8d6005870b49e5471954a14651d1b7be08d6d1
Lucas Mendes <lucas39lm@hotmail.com>
2024-01-27 15:05:14 -0300

    Icons-Texts Conditions and Humidity

--- src/WeatherAppGui.java
+++ src/WeatherAppGui.java
@@ -33,6 +33,31 @@ public class WeatherAppGui extends JFrame {
         searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         searchButton.setBounds(375, 13, 47, 45);
         add(searchButton);
+
+        JLabel weatherConditionImage = new JLabel(loadImage("src/images/cloud.png"));
+        weatherConditionImage.setBounds(0, 125, 450, 217);
+        add(weatherConditionImage);
+
+        JLabel temperatureText = new JLabel("10 C");
+        temperatureText.setBounds(0, 350, 450, 54);
+        temperatureText.setFont(new Font("Dialog", Font.BOLD, 48));
+        temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
+        add(temperatureText);
+
+        JLabel weatherConditionDescription = new JLabel("Cloudy");
+        weatherConditionDescription.setBounds(0, 405, 450, 36);
+        weatherConditionDescription.setFont(new Font("Dialog", Font.PLAIN, 32));
+        weatherConditionDescription.setHorizontalAlignment(SwingConstants.CENTER);
+        add(weatherConditionDescription);
+
+        JLabel humidityImage = new JLabel(loadImage("src/images/umidade.png"));
+        humidityImage.setBounds(12, 510, 74, 66);
+        add(humidityImage);
+
+        JLabel humidityText = new JLabel("<html><b>Humidity</b> 100%</html>");
+        humidityText.setBounds(75, 512, 85, 55);
+        humidityText.setFont(new Font("Dialog", Font.PLAIN, 16));
+        add(humidityText);
     }
 
     private ImageIcon loadImage(String resourcePath){
